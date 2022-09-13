b3657086afcef572e0a774b3cc3682fe502f3678
rak1b <rak13.dev@gmail.com>
2022-09-13 21:02:15 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter as Router} from "react-router-dom";
+import { Routes, Route } from "react-router";
+// import Dashboard from "./modules/dashboard/component/Dashboard";
+
+import Header from "./navbar/component/Header"
 
 function App() {
+  const [bodyWidth, setBodyWidth] = useState(false);
+  const [hideToolbar, setHideToolbar] = useState(false);
   return (
-    <div >
-      <p>Hlw</p>
+    <div>
+      <Router>
+        <div
+          className={` ${
+            !bodyWidth && !hideToolbar
+              ? "main-content"
+              : !hideToolbar
+              ? "main-content-collapsed"
+              : ""
+          }`}
+        >
+          {!hideToolbar && <Header setBodyWidth={setBodyWidth} />}
+          <div>
+          </div>
+        </div>
+      </Router>
     </div>
   );
 }
